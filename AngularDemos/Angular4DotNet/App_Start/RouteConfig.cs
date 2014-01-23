6b8e83d0af8951487f01c152d00570c16ccc216b
Abhishek Jain <abhi2000@gmail.com>
2014-01-23 17:30:41 +0000

    Angular4DotNet - Bootstrapping data

--- AngularDemos/Angular4DotNet/App_Start/RouteConfig.cs
+++ AngularDemos/Angular4DotNet/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Angular4DotNet
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Hello", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Courses", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
