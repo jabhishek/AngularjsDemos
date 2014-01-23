75364efb4318e9ad005c9bdf7815a3682362a8b6
Abhishek Jain <abhi2000@gmail.com>
2014-01-23 17:32:30 +0000

    Add missing files

---
+++ AngularDemos/Angular4DotNet/Controllers/CoursesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Angular4DotNet.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Index()
+        {
+            return View("Index", "", GetCourses());
+        }
+
+        public string GetCourses()
+        {
+            var courses = new[]
+            {
+                new Course {Number= "1", Name = "C#", Instructor= "Scott Allen"},
+                new Course {Number= "2", Name = "Angularjs", Instructor= "Joe Eames"},
+                new Course {Number= "3", Name = "Javascript", Instructor= "Elijah Manor"}
+            };
+            var settings = new JsonSerializerSettings{ContractResolver = new CamelCasePropertyNamesContractResolver()};
+            return JsonConvert.SerializeObject(courses, Formatting.None, settings);
+        }
+
+    }
+
+    public class Course
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+    }
+
+}
